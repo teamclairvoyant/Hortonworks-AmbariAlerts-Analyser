dbdc968dd4d704b8eb2b1c0b52d6c5e53742b69d
sorabhgit <47589194+sorabhgit@users.noreply.github.com>
2019-07-25 15:06:33 +0530

    Create Fetch_ServiceDown_History.sql
    Run this SQL query to check what and how long Hadoop services were down in last 7 Days .
    - this query will use alert_history table from ambari postgres database .
    - filter out alert_definition_id as per your needs .
    - check definition_id from alert_definitions table .

---
+++ Fetch_ServiceDown_History.sql
@@ -0,0 +1,20 @@
+SELECT q.component_name,
+       q.host_name,
+       q.prev_update_at downtime_start_time,
+       q.alert_timestamp2 downtime_end_time,
+       q.alert_timestamp2- q.prev_update_at downtime
+FROM   (
+                SELECT   component_name,
+                         host_name,
+                         To_timestamp(alert_timestamp/1000) alert_timestamp2 ,
+                         alert_label,
+                         alert_state,
+                         Lag(To_timestamp(alert_timestamp/1000)) OVER( partition BY component_name,host_name ORDER BY To_timestamp(alert_timestamp/1000) ) AS prev_update_at
+                FROM     ambari.alert_history
+                WHERE    To_timestamp(alert_timestamp/1000) > CURRENT_DATE - 7
+--Dev           AND alert_definition_id in (2,4,13,28,33,42,51,53,55,71,72,54,152,151,252,107,322,335,338)
+--Prod	        AND alert_definition_id in (3, 13, 17, 35, 41, 68, 40, 42, 43, 56, 51, 55, 81,122, 19, 18, 20, 44,221,171, 54, 48,  1,275,278)
+                ORDER BY component_name,host_name,alert_timestamp) q
+                WHERE q.alert_state like '%OK%'
+                and q.prev_update_at is not null
+                ORDER BY q.component_name,q.host_name,q.alert_timestamp2;
